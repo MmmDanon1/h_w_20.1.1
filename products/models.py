987b10e46d4cf481f020af43cd6e79977ca868f6
Yura <Orl0ff.1ur@yandex.ru>
2023-10-03 14:36:29 +0300

    Initial commit

---
+++ products/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=250, verbose_name='название')
+    description = models.TextField(**NULLABLE, verbose_name='описание')
+
+    def __str__(self):
+        return f'{self.name}, {self.description}'
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=250, verbose_name='название')
+    description = models.TextField(**NULLABLE, verbose_name='описание')
+    image = models.ImageField(upload_to='products/', **NULLABLE, verbose_name='изображение')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='категория')
+    price = models.IntegerField(verbose_name='цена')
+    date_created = models.DateTimeField(**NULLABLE, verbose_name='дата создания')
+    date_of_change = models.DateTimeField(**NULLABLE, verbose_name='дата полследнего изменения')
+
+    def __str__(self):
+        return f'{self.name}, {self.description}'
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
