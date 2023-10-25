1cd1e77e103687a5e9c05df3fc517e5acafed279
Yura <Orl0ff.1ur@yandex.ru>
2023-10-26 00:08:23 +0300

    creat dir static

--- products/views.py
+++ products/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpRequest, HttpResponse
+from datetime import datetime
+from products.models import Product, Category
 
-# Create your views here.
+
+def product(request: HttpRequest):
+    """представление страницы main/product.html для каждого продукта"""
+    products_list = Product.objects.all()
+    context = {
+        "object_list": products_list
+    }
+    return render(request, 'products/product.html', context)
