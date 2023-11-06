aac98c3ca84c2d4f554f2c77ce3eedce85ef9d10
Yura <Orl0ff.1ur@yandex.ru>
2023-11-07 01:51:14 +0300

    creat 20.2

--- products/urls.py
+++ products/urls.py
@@ -7,10 +7,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', views.home, name='home'),
     path('product/<int:product_id>/', views.product, name='product')
-    # path('contact/', views.contact, name='contact'),
-    # path('contacts/', views.contacts, name='contacts'),
-    # path('great_prod/', views.great_prod, name='great_prod'),
-
 ]
 
 if settings.DEBUG:
