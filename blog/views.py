9ec63ea62deafbb7812f64cb3c3f7f6bc3cf4573
Yura <Orl0ff.1ur@yandex.ru>
2023-11-08 00:52:48 +0300

    creat 21.1

--- blog/views.py
+++ blog/views.py
@@ -1,22 +1,23 @@
 from django.views.generic import CreateView, UpdateView, DeleteView, DetailView, ListView
 from .models import Blog
+from django.urls import reverse_lazy
 
 
 class BlogCreateView(CreateView):
     model = Blog
     fields = ['name', 'slug', 'description', 'preview', 'is_publish']
-    success_url = '/blog/'
+    success_url = reverse_lazy('blog_list')
 
 
 class BlogUpdateView(UpdateView):
     model = Blog
     fields = ['name', 'slug', 'description', 'preview', 'is_publish']
-    success_url = '/blog/'
+    success_url = reverse_lazy('blog_list')
 
 
 class BlogDeleteView(DeleteView):
     model = Blog
-    success_url = '/blog/'
+    success_url = reverse_lazy('blog_list')
 
 
 class BlogDetailView(DetailView):
